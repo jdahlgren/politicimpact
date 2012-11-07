0e1c3ae626cb296605ef0b76399e29a18c08a8db
Johannes Dahlgren <johda876@student.liu.se>
2012-11-07 11:21:27 +0100

    items added to homescreen dropdown menu

--- PoliticImpact/Controllers/HomeController.cs
+++ PoliticImpact/Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PoliticImpact.Models;
 
 namespace PoliticImpact.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICaseItemRepository caseitemRepository = new CaseItemRepository();
+
         public ActionResult Index()
-        {
+        {            
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+            return View(caseitemRepository.All);
         }
 
         public ActionResult About()
